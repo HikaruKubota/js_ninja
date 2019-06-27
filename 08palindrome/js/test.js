6587a47ddbf6c00e1d4f869b3671dd4d85c5b717
kubota <sakaku193@gmail.com>
2019-06-27 14:57:02 +0900

    回文

--- 08palindrome/js/test.js
+++ 08palindrome/js/test.js
@@ -1,9 +1,14 @@
 (function(){
-  let values = [213, 16, 2058, 54, 10, 1965, 57, 9];
-  //ソートについてというか関数について理解が浅い。。。
-  //aにはどのタイミングで何が入るのかーとか
-  //リターンしているのは分かるがなぜそれでソートされるのかーとかが分からない。
-  values.sort(function(a, b){
-    return a - b;
-  });
+  let test = "しんぶんし";
+
+  function isPalindrome(word){
+    if(word.length <= 1){
+      return true;
+    }
+    if(word.charAt(0) != word.charAt(word.length-1)){
+      return false;
+    }
+    return isPalindrome(word.substr(1,word.length-2));
+  }
+  console.log(isPalindrome(test));
 })();
